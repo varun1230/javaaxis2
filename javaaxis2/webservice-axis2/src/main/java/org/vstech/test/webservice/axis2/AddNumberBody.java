224ec092e58e1aad60179f411597b34d129f6f12
Varun Dosapati <varun.dosapati@courts.mo.gov>
2018-09-24 17:03:50 -0500

    Made few more changes to fix to work(Need to work on adding spring security)

--- javaaxis2/webservice-axis2/src/main/java/org/vstech/test/webservice/axis2/AddNumberBody.java
+++ javaaxis2/webservice-axis2/src/main/java/org/vstech/test/webservice/axis2/AddNumberBody.java
@@ -1,9 +1,18 @@
 package org.vstech.test.webservice.axis2;
 
-public class AddNumberBody extends AddNumbersServiceSkeleton implements AddNumbersServiceSkeletonInterface {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+public class AddNumberBody extends WebserviceTest1Skeleton implements WebserviceTest1SkeletonInterface, InitializingBean {
+	private static final Logger logger = LoggerFactory.getLogger(AddNumberBody.class);
 	
 	 public int addNumbers(int arg0, int arg1) throws AddNumbersFault {
 		 return arg0+arg1;
 	 }
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+	}
 	 
 }
