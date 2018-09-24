f26830c65d3d76ef870c2efcb1283996031a8df4
Varun Dosapati <varun.dosapati@courts.mo.gov>
2018-09-24 17:03:50 -0500

    Made few more changes for client

--- javaaxis2/webservice-axis2/src/main/java/org/vstech/test/webservice/axis2/spring/SpringInit.java
+++ javaaxis2/webservice-axis2/src/main/java/org/vstech/test/webservice/axis2/spring/SpringInit.java
@@ -3,11 +3,16 @@ package org.vstech.test.webservice.axis2.spring;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.description.AxisService;
 import org.apache.axis2.engine.ServiceLifeCycle;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.axis2.extensions.spring.receivers.ApplicationContextHolder;
+import org.apache.axis2.extensions.spring.util.ApplicationContextUtil;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+//import com.ibatis.common.resources.Resources;
 
 public class SpringInit implements ServiceLifeCycle {
 
-	@Override
 	public void startUp(ConfigurationContext configctx, AxisService service) {
 		try {
 			System.out.println("inside webservice-axis2 spinginit");
@@ -16,38 +21,34 @@ public class SpringInit implements ServiceLifeCycle {
 
 			// NOT SURE WHY THIS NEEDS TO HAPPEN BUT IT DOES
 			Thread.currentThread().setContextClassLoader(classLoader);
-
+			
+//			Resources.setDefaultClassLoader(classLoader);
 			// LOAD THE SPRING CONFIG AND UPDATE THE CLASSLOADER
 
 			System.out.println("Starting spring init");
-			ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext(
-					new String[] { "spring-config-main.xml" }, false);
-			appCtx.setClassLoader(classLoader);
-			appCtx.refresh();
+//			ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext(
+//					new String[] { "spring-config-main.xml" }, false);
+//			appCtx.setClassLoader(classLoader);
+//			appCtx.refresh();
 			System.out.println("spring loaded");
 
 			// String location = "spring-config-main.xml";
 			//
-			// GenericApplicationContext applicationContext = new
-			// GenericApplicationContext();
-			// applicationContext.setClassLoader(classLoader);
-			//
-			//
-			// XmlBeanDefinitionReader xmlReader = new
-			// XmlBeanDefinitionReader(applicationContext);
-			// xmlReader.loadBeanDefinitions(new ClassPathResource(location));
-			//
-			// applicationContext.refresh();
-			//
-			// service.addParameter(ApplicationContextUtil.SPRING_APPLICATION_CONTEXT,
-			// ApplicationContextHolder.getContext());
+			 GenericApplicationContext applicationContext = new GenericApplicationContext();
+			 applicationContext.setClassLoader(classLoader);
+			
+			
+			 XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(applicationContext);
+			 xmlReader.loadBeanDefinitions(new ClassPathResource("spring-config-main.xml"));
+			 applicationContext.refresh();
+			
+			 service.addParameter(ApplicationContextUtil.SPRING_APPLICATION_CONTEXT, ApplicationContextHolder.getContext());
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	@Override
 	public void shutDown(ConfigurationContext configctx, AxisService service) {
 
 	}
